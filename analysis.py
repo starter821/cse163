a6db1e4499bf3468e177583acd073b7e237c616c
Sunghee Park <sungheepark@Sunghees-MacBook-Pro.local>
2023-03-12 19:11:31 -0700

    updated

--- analysis.py
+++ analysis.py
@@ -9,13 +9,13 @@ we used to create the visualization for our research questions and analysis.
 
 import pandas as pd
 
-from question1 import gun_and_unemployment
+from question1 import gun_and_unemployment_line, gun_and_unemployment_scatter
 from question2 import gun_and_crime
 from question3 import create_pie_chart
 
-gun_violence_df = pd.read_csv('cse163/incident.csv')
-unemployment_df = pd.read_csv('cse163/unemployment_rate_data.csv')
-violent_crime_df = pd.read_csv('cse163/state_crime.csv')
+gun_violence_df = pd.read_csv('incident.csv')
+unemployment_df = pd.read_csv('unemployment_rate_data.csv')
+violent_crime_df = pd.read_csv('state_crime.csv')
 
 # year, month, and day from unemployment dataframe
 unemployment_df['year'] = pd.DatetimeIndex(unemployment_df['date']).year
@@ -32,7 +32,8 @@ gun_violence_df['day'] = pd.DatetimeIndex(gun_violence_df['Incident_Date']).day
 
 def main():
     # Quesiton 1
-    gun_and_unemployment(gun_violence_df, unemployment_df)
+    gun_and_unemployment_line(gun_violence_df, unemployment_df)
+    gun_and_unemployment_scatter(gun_violence_df, unemployment_df)
 
     # Question 2
     gun_and_crime(gun_violence_df, violent_crime_df)
--- question1.py
+++ question1.py
@@ -10,32 +10,17 @@ correlation between national unemployment rate and gun violence from
 """
 
 
-import math
-
 import pandas as pd
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
 
 from matplotlib import pyplot as plt
 from plotly.subplots import make_subplots
 
 
-gun = pd.read_csv('cse163/incident.csv')
-unemployment = pd.read_csv('cse163/unemployment_rate_data.csv')
-
-# year, month, and day from unemployment dataframe
-unemployment['year'] = pd.DatetimeIndex(unemployment['date']).year
-unemployment['month'] = pd.DatetimeIndex(unemployment['date']).month
-unemployment['day'] = pd.DatetimeIndex(unemployment['date']).day
-
-# year, month, and day from gun violence dataframe
-gun['year'] = pd.DatetimeIndex(gun['Incident_Date']).year
-gun['month'] = pd.DatetimeIndex(gun['Incident_Date']).month
-gun['day'] = pd.DatetimeIndex(gun['Incident_Date']).day
-
-
-def gun_and_unemployment(gun: pd.DataFrame, unemployment: pd.DataFrame) -> None:
+def gun_and_unemployment_line(gun: pd.DataFrame, unemployment: pd.DataFrame) -> None:
     '''
     This method takes in two dataframes, secifically the gun violence dataset
     and unemployment dataset. It then cre
@@ -88,4 +73,135 @@ def gun_and_unemployment(gun: pd.DataFrame, unemployment: pd.DataFrame) -> None:
     )
     fig.update_layout({'xaxis2': {'side': 'top'}})
 
+    fig.show()
+
+
+def gun_and_unemployment_scatter(gun: pd.DataFrame, unemployment: pd.DataFrame) -> None:
+
+    # add 'both' column that contains the sum of 'Injured' and 'Killed' column
+    gun['both'] = gun['Injured'] + gun['Killed']
+
+    # group them by year & month
+    gun2 = gun.groupby(['year', 'month'], as_index=False)['both'].sum()
+    unemployment2 = unemployment.groupby(
+        ['year', 'month'], as_index=False)['unrate'].mean()
+
+    # create date column with format of YYYY-MM
+    gun2['date'] = gun2['year'].astype(str) + "-" + gun2['month'].astype(str)
+    unemployment2['date'] = unemployment2['year'].astype(
+        str) + "-" + unemployment2['month'].astype(str)
+
+    unemployment2['date'] = pd.to_datetime(unemployment2['date'])
+    unemployment2['date'] = unemployment2['date'].dt.strftime('%Y-%m')
+    unemployment2 = unemployment2.sort_values(by=['date'])
+
+    gun2['date'] = pd.to_datetime(gun2['date'])
+    gun2['date'] = gun2['date'].dt.strftime('%Y-%m')
+    gun2 = gun2.sort_values(by=['date'])
+
+    # filter unemployment from 2014 - 2019
+    year_mask_2019 = (unemployment2['year'] >= 2014) & (
+        unemployment2['year'] <= 2019)
+    year_mask_2021 = (unemployment2['year'] >= 2014) & (
+        unemployment2['year'] <= 2021)
+    unemployment_2019 = unemployment2[year_mask_2019]
+    unemployment_2021 = unemployment2[year_mask_2021]
+
+    # filter gun_violence from 2014 - 2021
+    year_mask_2019 = (gun2['year'] >= 2014) & (gun2['year'] <= 2019)
+    year_mask_2021 = (gun2['year'] >= 2014) & (gun2['year'] <= 2021)
+    gun_2019 = gun2[year_mask_2019]
+    gun_2021 = gun2[year_mask_2021]
+
+    # join data -- automatically drops na
+    data_2019 = gun_2019.merge(unemployment_2019, on=['date', 'year', 'month'])
+    data_2021 = gun_2021.merge(unemployment_2021, on=['date', 'year', 'month'])
+
+    # Initialize figure
+    fig = go.Figure()
+
+    # Add Traces
+    fig.add_trace(
+        go.Scatter(x=data_2019['both'],
+                   y=data_2019['unrate'],
+                   mode='markers',
+                   marker=dict(
+            color=data_2019['year'],
+            colorscale='Viridis',
+            showscale=True,
+        ),
+            showlegend=False,
+        )
+    )
+
+    fig.add_trace(
+        go.Scatter(x=data_2021['both'],
+                   y=data_2021['unrate'],
+                   mode='markers',
+                   marker=dict(
+            color=data_2021['year'],
+            colorscale='Viridis',
+            showscale=True
+        ),
+            showlegend=False
+        )
+    )
+
+    # # Calculate the best-fit lines
+    z1 = np.polyfit(data_2019['both'], data_2019['unrate'], 1)
+    p1 = np.poly1d(z1)
+
+    z2 = np.polyfit(data_2021['both'], data_2021['unrate'], 1)
+    p2 = np.poly1d(z2)
+
+    # add best-fit lines
+    fig.add_trace(
+        go.Scatter(
+            x=data_2019['both'],
+            y=p1(data_2019['both']),
+            name='Trend line 1',
+            line=dict(color='black'),
+            showlegend=False,
+        )
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=data_2021['both'],
+            y=p1(data_2021['both']),
+            name='Trend line 2',
+            line=dict(color='black'),
+            showlegend=False
+        )
+    )
+
+    # add labels
+    fig.update_layout(
+        title='Correlation between Gun Violence and Unemployment Rate',
+        xaxis=dict(
+            title="Number of Gun Violence"
+        ),
+        yaxis=dict(
+            title="Unemployment Rate"
+        )
+    )
+
+    # menus
+    fig.update_layout(
+        updatemenus=[
+            dict(
+                active=0,
+                buttons=list([
+                    dict(label="2014 - 2019",
+                         method="update",
+                         args=[{"visible": [True, False]}]),
+                    dict(label="2014 - 2021",
+                         method="update",
+                         args=[{"visible": [False, True]}]),
+                ]),
+            )
+        ])
+
+    fig.data[0].visible = True
+    fig.data[1].visible = False
     fig.show()
